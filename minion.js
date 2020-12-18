29a91431c7c83053c94c4da2bbd00694b510f637
Daksh Dudhat <62796512+dakshdudhat@users.noreply.github.com>
2020-12-18 17:49:07 +0530

    photo

--- minion.js
+++ minion.js
@@ -1,8 +1,34 @@
-var input = document.querySelector("#btn-translate")
+var button = document.querySelector("#btn-translate")
+var outTxt = document.querySelector("#txt-output")
+var intxt = document.querySelector("#txt-input");
 
-var click = input.addEventListener("click",callback);
+
+var serverUrl = "https://api.funtranslations.com/translate/minion.json"
+
+
+function getTranslationURL(input) {
+    return serverUrl + "?" + "text=" + input;
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
+}
+
 
 
 function callback() {
-    console.log("clicked");
-}
+    var inputText = intxt.value;
+
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outTxt.innerText = translatedText; // output
+        })
+        .catch(errorHandler)
+
+
+};
+
+button.addEventListener("click", callback);
